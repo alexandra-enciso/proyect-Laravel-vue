ffcfb8b33103ddeca43b4859940b5016785e9f95
Alexandra Enciso <alexandra.enciso@iudigital.edu.co>
2023-03-12 11:13:12 -0500

    inocio bootstrap

---
+++ paginas/controller/controller.php
@@ -0,0 +1,20 @@
+<?php
+// your code here!
+echo "hola mundo"
+$mivariable = "jose";
+echo
+$mivariable = 5;
+echo $mivariable;
+int $mivariable = 6.54;
+echo 
+$a = 5;
+$b = 12
+$c = "hola";
+$suma = $a + $b;
+echo $suma;
+
+
++ $c;
+
+
+?>
